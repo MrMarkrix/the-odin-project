049ca24863707bc9b0d16588a85ba07c88f24bea
MrMarkrix <main.markrix@gmail.com>
2023-07-18 21:44:25 +0200

    Create Secondary Branch, Edit Project Navigator Page, Style Project Navigator Page

--- 03-Project-RockPaperScissors/script.js
+++ 03-Project-RockPaperScissors/script.js
@@ -1,94 +1,92 @@
 let userScore = 0;
 let computerScore = 0;
 
-function theGame(){
+const choicesContainer = document.getElementById('choices');
+const resultElement = document.querySelector('.result');
+const userScoreElement = document.querySelector('.userScore');
+const computerScoreElement = document.querySelector('.computerScore');
+const resetButton = document.getElementById('reset');
 
-  function computerRandom() {
+function theGame() {
+  let userInput = "";
+  let computerRandom = "";
+
+  function UserChoice() {
+    const choices = document.querySelectorAll('#choices button');
+    choices.forEach((choice) => {
+      choice.addEventListener('click', () => {
+        userInput = choice.textContent;
+        computerChoice();
+        playRound();
+      });
+    });
+  }
+
+  function computerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+    computerRandom = choices[randomIndex];
+  }
 
-    const randomRandom = Math.floor(Math.random() * choices.length);
-
-    return choices[randomRandom];
-  };
-
-  //The function generates a random index using Math.random() 
-  //multiplied by the length of the choices array (choices.length)
-  //It then returns the string value at the randomly generated index from the choices array.
-
-  function userInput() {
-    const choices = ["Rock", "Paper", "Scissors"];
-    let userInput = prompt("Choose your weapon (Rock, Paper, Scissors)!:");
-    
-    // Validate user input,
-    // while creates a loop, that checks if userInput is within Choices
-    // if not the loop again
-    
-    while (choices.includes(userInput) === false) {
-      userInput = prompt("Input is Case-sensetive. Please choose Rock, Paper, or Scissors:");
+  function playRound() {
+    if (userScore === 3 || computerScore === 3) {
+      return; // Stop further execution if either score reaches 3
     }
-    return userInput;
-  };
 
-  //Function prompts the user to choose a weapon (Rock, Paper, or Scissors)
-  //It then validates the user"s input by checking if it is included in the choices array
-  //If the user enters an invalid input, the function will continue to prompt until a valid input is provided.
+    console.log("User: ", userInput);
+    console.log("Computer: ", computerRandom);
 
-while (userScore <3 && computerScore <3){
-
-  const computerChoice = computerRandom();
-  const userChoice = userInput();
-
-  console.log ("User: ", userChoice);
-  console.log ("Computer:", computerChoice);
-
-  if (userChoice === computerChoice) {
-    alert("It's a draw!"); 
-    console.log("It's a draw!");}
-    
-    else if (userChoice === "Rock" && computerChoice === "Scissors") {
+    if (userInput === computerRandom) {
+      resultElement.textContent = "It's a draw!";
+      console.log("It's a draw!");
+    } else if (userInput === "Rock" && computerRandom === "Scissors") {
       console.log("You Win! Your Rock smashed the Scissors!");
-      alert("You Win! Your Rock smashed the Scissors!");
+      resultElement.textContent = "You Win! Your Rock smashed the Scissors!";
       userScore++;
-    }
-    
-    else if (userChoice === "Rock" && computerChoice === "Paper") {
+    } else if (userInput === "Rock" && computerRandom === "Paper") {
       console.log("You Lose! Your Rock was wrapped by Paper!");
-      alert("You Lose! Your Rock was wrapped by Paper!");
-      computerScore++;c
-    }
-
-    else if (userChoice === "Paper" && computerChoice === "Rock") {
+      resultElement.textContent = "You Lose! Your Rock was wrapped by Paper!";
+      computerScore++;
+    } else if (userInput === "Paper" && computerRandom === "Rock") {
       console.log("You Win! Your Paper wrapped the Rock!");
-      alert("You Win! Your Paper wrapped the Rock!");
+      resultElement.textContent = "You Win! Your Paper wrapped the Rock!";
       userScore++;
-    } 
-      
-    else if (userChoice === "Paper" && computerChoice === "Scissors") {
-      console.log("You Lose! Your Paper was sciced by Scissors!");
-      alert("You Lose! Your Paper was sciced by Scissors!");
+    } else if (userInput === "Paper" && computerRandom === "Scissors") {
+      console.log("You Lose! Your Paper was sliced by Scissors!");
+      resultElement.textContent = "You Lose! Your Paper was sliced by Scissors!";
+      computerScore++;
+    } else if (userInput === "Scissors" && computerRandom === "Paper") {
+      console.log("You Win! Your Scissors sliced the Paper!");
+      resultElement.textContent = "You Win! Your Scissors sliced the Paper!";
+      userScore++;
+    } else if (userInput === "Scissors" && computerRandom === "Rock") {
+      console.log("You Lose! Your Scissors were smashed by Rock!");
+      resultElement.textContent = "You Lose! Your Scissors were smashed by Rock!";
       computerScore++;
     }
 
-    else if (userChoice === "Scissors" && computerChoice === "Paper") {
-      console.log("You Win! Your Scissors sliced the Paper!");
-      alert("You Win! Your Scissors sliced the Paper!");
-      userScore++;
-    } 
-        
-    else if (userChoice === "Scissors" && computerChoice === "Rock") {
-      console.log("You Lose! Your Scissors were smashed by Rock!");
-      alert("You Lose! Your Scissors were smashed by Rock!");
-      computerScore++;
+    userScoreElement.textContent = userScore.toString();
+    computerScoreElement.textContent = computerScore.toString();
+
+    if (userScore === 3) {
+      resultElement.textContent = "Congratulations! You won the game! Check the Console for results.";
+      console.log("Congratulations! You won the game!");
+    } else if (computerScore === 3) {
+      resultElement.textContent = "Oops! The Computer won the game! Check the Console for results.";
+      console.log("Oops! The Computer won the game!");
     }
   }
-  
-  if (userScore === 3) {
-    alert("Congratulations! You won the game! Check the Console for results.");
-    console.log("Congratulations! You won the game!");
-  } else {
-    alert("Oops! The Computer won the game! Check the Console for results.");
-    console.log("Oops! The Computer won the game!");
+
+  function resetGame() {
+    userScore = 0;
+    computerScore = 0;
+    userScoreElement.textContent = "0";
+    computerScoreElement.textContent = "0";
+    resultElement.textContent = "Result";
   }
+
+  UserChoice();
+  resetButton.addEventListener('click', resetGame);
 }
 
-theGame()
+theGame();
